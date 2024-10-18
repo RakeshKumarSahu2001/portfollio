8e6492b838faafbd0c23a848f13c5df796fda4bc
RakeshKumarSahu2001 <rakeshkumarsahu187@gmail.com>
2024-10-18 15:45:03 +0530

    add some style to the navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,20 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../App.css";
 
 function Navbar() {
     const [toggle, setToggle] = useState(false);
     const [isActive, setIsActive] = useState("Home");
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
     const handleToggle = () => {
         setToggle((prev) => !prev);
     };
 
-    const handleNavItemClick = (itemName:string) => {
+    const handleNavItemClick = (itemName: string) => {
         setIsActive(itemName);
         if (window.innerWidth < 768) {
             setToggle(false);
         }
     };
+
+    useEffect(() => {
+        const handleWindowWidth = () => {
+            setWindowWidth(window.innerWidth)
+        }
+
+        window.addEventListener("resize", handleWindowWidth)
+        return () => {
+            window.addEventListener("resize", handleWindowWidth)
+
+        }
+    }, [])
 
 
     const navItems = [
@@ -27,18 +40,18 @@ function Navbar() {
 
     return (
         <nav className="navbar nav-box navbar-expand-sm navbar-dark bg-dark z-3 position-fixed" style={{ height: "5rem", width: "100%" }} aria-label="Third navbar example">
-            <div className="container nav-container">
+            <div className="container nav-container position-relative">
                 <a className="navbar-brand fs-3 fw-semibold" href="#">Rakesh.</a>
 
                 <button className={`navbar-toggler ${!toggle && "collapsed"}`} type="button" onClick={handleToggle} data-bs-toggle="collapse" data-bs-target="#port-nav" aria-controls="port-nav" aria-expanded={toggle} aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className={`collapse navbar-collapse link-box ${toggle && "show"}`} id="port-nav">
-                    <ul className="navbar-nav ms-auto mb-2 mb-sm-0 d-flex gap-1">
+                <div className={`collapse navbar-collapse link-box ${toggle ? "show nav-show-position glass-morphism" :""} ${windowWidth < 576 ? "position-absolute w-100 nav-list-position" : ""} `} id="port-nav">
+                    <ul className="navbar-nav px-3 px-sm-0 ms-auto mb-2 mb-sm-0 d-flex gap-1">
                         {navItems.map((item) => (
-                            <li className="nav-item" key={item.name} onClick={() => handleNavItemClick(item.name)}>
-                                <a 
+                            <li className={`nav-item nav-item-hover ${windowWidth < 576 ? "" : ""}`} key={item.name} onClick={() => handleNavItemClick(item.name)}>
+                                <a
                                     className={`nav-link ${isActive === item.name ? "active" : ""}`}
                                     style={{
                                         fontSize: isActive === item.name ? "17px" : undefined,
